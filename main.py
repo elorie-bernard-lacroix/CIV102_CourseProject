048624107185ab75962362ff6755bcaa3fbdc538
Elorie Bernard-Lacroix <eloriel0310@gmail.com>
2022-11-11 23:24:15 -0500

    a few fixes

--- main.py
+++ main.py
@@ -21,12 +21,7 @@ def shear_force(cut):
     global r_a, r_b
 
     V = 0
-    if cut >= span:
-        V = r_a - 400 + r_b
-    elif cut >= xtrain:
-        V = r_a - 400
-    else:
-        V = r_a
+    
 
     return V
 
@@ -39,23 +34,9 @@ def bending_moment(cut):
     global r_a, r_b
 
     M = 0
-    if cut >= span:
-        M = 0
-    elif cut >= xtrain:
-        M = xtrain*r_a - (cut-xtrain)*(r_a-400)
-    else:
-        M = xtrain*r_a
+    
 
     return M
-
-def required_SF():
-    '''
-    This function collects the SF forces at all points necessary to plot the diagram.
-    '''
-    pass
-
-def required_BM():
-    pass
 
 def plot_BMD():
     pass
@@ -66,3 +47,15 @@ def plot_SFD():
 if '__name__' == '__main__':
     xtrain = input("starting location of train (0 ≤ xtrain ≤ 240 mm): ")
     span = 1200 # span of bridge in mm
+    load = 400/6
+
+    d1 = xtrain + 52
+    d2 = xtrain + 228
+    d3 = xtrain + 392
+    d4 = xtrain + 568
+    d5 = xtrain + 732
+    d6 = xtrain + 784
+
+    reaction_forces()
+
+
